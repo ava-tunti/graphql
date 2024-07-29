101017c6aed7fa66e397bf024ee65ce058240399
Ava Tuntikanokporn <150287550+ava-tunti@users.noreply.github.com>
2024-07-28 18:26:15 -0700

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -196,7 +196,35 @@ const { buildSchema } = require('graphql');
 
 // Sample restaurant data
 let restaurants = [
-  // ... (your restaurant data here)
+   {
+    id: 1,
+    name: "WoodsHill",
+    description: "American cuisine, farm to table, with fresh produce every day",
+    dishes: [
+      { name: "Swordfish grill", price: 27 },
+      { name: "Roasted Broccoli", price: 11 },
+    ],
+  },
+  {
+    id: 2,
+    name: "Fiorellas",
+    description: "Italian-American home cooked food with fresh pasta and sauces",
+    dishes: [
+      { name: "Flatbread", price: 14 },
+      { name: "Carbonara", price: 18 },
+      { name: "Spaghetti", price: 19 },
+    ],
+  },
+  {
+    id: 3,
+    name: "Karma",
+    description: "Malaysian-Chinese-Japanese fusion, with great bar and bartenders",
+    dishes: [
+      { name: "Dragon Roll", price: 12 },
+      { name: "Pancake roll", price: 11 },
+      { name: "Cod cakes", price: 13 },
+    ],
+  },
 ];
 
 // Construct a schema, using GraphQL schema language
